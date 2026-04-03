01cbb2ae606a5d5eb432ca55085cf36c2f82184b
Alexander Freiherr von Buddenbrock <a.buddenbrock@gmail.com>
2026-04-03 12:07:13 +0200

    Add logic for Urgoz Splitway and an example .lss

--- tools/RunAnalyzer.py
+++ tools/RunAnalyzer.py
@@ -5,6 +5,8 @@ import os
 import datetime
 import xml.etree.cElementTree as ET
 
+combine_urgoz_zones = False
+
 def analyze_runs(path, run_type):
     runs = []
     for file in os.listdir(path):
@@ -18,6 +20,41 @@ def analyze_runs(path, run_type):
     successes = 0
     segments = {}
     best_run = None
+
+    if run_type == "Urgoz's Warren" and combine_urgoz_zones:
+        for run in runs:
+            offset = 0
+            # merge objectives 1 and 2 into one segment
+            if run["objectives"][0]["status"] == 2 and run["objectives"][1]["status"] == 2 and run["objectives"][2]["status"] == 2:
+                run["objectives"][0]["duration"] += run["objectives"][1]["duration"]
+                run["objectives"][0]["duration"] += run["objectives"][2]["duration"]
+                run["objectives"][0]["done"] = run["objectives"][2]["done"]
+                run["objectives"].pop(2)
+                run["objectives"].pop(1)
+                run["objectives"][0]["name"] = "Zone 1 + 2 + 3 | Weakness + Life Drain + Levers"
+                offset+=2
+            else:
+                # remove objectives 1 and 2 if they are not completed
+                run["objectives"].pop(2)
+                run["objectives"].pop(1)
+                run["objectives"].pop(0)
+                offset+=3
+                
+            
+            # merge objectives 7 and 8 and 9 into one segment (now 6 and 7 after the first merge)
+            if run["objectives"][6-offset]["status"] == 2 and run["objectives"][7-offset]["status"] == 2 and run["objectives"][8-offset]["status"] == 2:
+                run["objectives"][6-offset]["duration"] += run["objectives"][7-offset]["duration"]
+                run["objectives"][6-offset]["duration"] += run["objectives"][8-offset]["duration"]
+                run["objectives"][6-offset]["done"] = run["objectives"][8-offset]["done"]
+                run["objectives"].pop(8-offset)
+                run["objectives"].pop(7-offset)
+                run["objectives"][6-offset]["name"] = "Zone 7 + 8 + 9 | Exhaustion + Pillars + Blood Drinkers"
+            else:
+                # remove objectives 7 and 8 and 9 if they are not completed
+                run["objectives"].pop(8-offset)
+                run["objectives"].pop(7-offset)
+                run["objectives"].pop(6-offset)
+        run_type = "Urgoz's Warren (Combined Zones)"
 
     for run in runs:
         attempts += 1
